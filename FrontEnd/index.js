1cdee5f12b3af04cbcaa4d06aa08a3e659e25424
Natan Oliveira da Silva <natan_o_da_silva@outlook.com>
2024-04-23 23:21:26 -0300

    Update index.js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,12 @@
-function notifica(){
-    alert("Avaliação enviada.");
-}
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log("Avaliação do atendimento:\nNota "+e.target.getAttribute('data-avaliacao'));
+  }
+});
